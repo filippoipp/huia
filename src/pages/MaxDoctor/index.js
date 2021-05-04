731e86c99ceb3f57e176334b7b450dcecd766626
filippoipp <filippoipp@hotmail.com>
2021-05-04 14:56:51 -0300

    feat: add scrollview to maxDoctor page

--- src/pages/MaxDoctor/index.js
+++ src/pages/MaxDoctor/index.js
@@ -1,21 +1,23 @@
+import React from "react";
+import { ScrollView } from 'react-native';
 
-import React from 'react';
+import * as S from "./styles";
 
-import * as S from './styles'
-
-import Header from '../../components/Header'
-import Form from '../../components/Form'
-import Icons from '../../components/Icons'
+import Header from "../../components/Header";
+import Form from "../../components/Form";
+import Icons from "../../components/Icons";
 
 const MaxDoctor = () => {
   return (
-    <S.Container>
-      <S.Background source={require("../../assets/Background.png")}>
-        <Header />
-        <Form />
-        <Icons />
-      </S.Background>
-    </S.Container>
+      <ScrollView>
+        <S.Container>
+          <S.Background source={require("../../assets/Background.png")}>
+            <Header />
+            <Form />
+            <Icons />
+          </S.Background>
+        </S.Container>
+      </ScrollView>
   );
 };
 
